31dd96ed25285772b03f5c8a0a0ebcf135946ef1
Kelly Luck <toymaker@usa.net>
2024-04-13 00:07:17 -0500

    Add project files.

---
+++ OKZoomer/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace OKZoomer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lvImages = new System.Windows.Forms.ListView();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numTweenFrames = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTweenFrames)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(156, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Image Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(12, 415);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(156, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lvImages
+            // 
+            this.lvImages.HideSelection = false;
+            this.lvImages.Location = new System.Drawing.Point(12, 41);
+            this.lvImages.Name = "lvImages";
+            this.lvImages.Size = new System.Drawing.Size(156, 368);
+            this.lvImages.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lvImages.TabIndex = 2;
+            this.lvImages.UseCompatibleStateImageBehavior = false;
+            this.lvImages.View = System.Windows.Forms.View.List;
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(174, 87);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(614, 322);
+            this.pic.TabIndex = 3;
+            this.pic.TabStop = false;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(515, 425);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(273, 13);
+            this.lblProgress.TabIndex = 4;
+            this.lblProgress.Text = "Select the directory with the files and press \'Go\' to begin.";
+            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number of tween frames:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numTweenFrames
+            // 
+            this.numTweenFrames.Location = new System.Drawing.Point(340, 20);
+            this.numTweenFrames.Name = "numTweenFrames";
+            this.numTweenFrames.Size = new System.Drawing.Size(49, 20);
+            this.numTweenFrames.TabIndex = 7;
+            this.numTweenFrames.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "File name prefix:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Location = new System.Drawing.Point(340, 55);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(100, 20);
+            this.txtPrefix.TabIndex = 9;
+            this.txtPrefix.Text = "test";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numTweenFrames);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.lvImages);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTweenFrames)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ListView lvImages;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numTweenFrames;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrefix;
+    }
+}
+
---
+++ OKZoomer/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OKZoomer
+{
+    public partial class Form1 : Form
+    {
+        string imagesSourcePath = @"F:\AIArt\output\img2img-images\2024-04-09\test";
+        string destinationPath = @"F:\AIArt\output\img2img-images\2024-04-09\test\out";
+        List<String> sourceImages;
+        int pictureCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = imagesSourcePath;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                imagesSourcePath = folderBrowserDialog1.SelectedPath;
+                LoadImages(imagesSourcePath);
+            }
+        }
+
+        private void LoadImages(string imagePath)
+        {
+            sourceImages = new List<string>();
+            lvImages.Items.Clear();
+            string[] fileEntries = Directory.GetFiles(imagePath);
+            foreach (string fileName in fileEntries)
+            {
+                sourceImages.Add(fileName);
+                lvImages.Items.Add(fileName);
+            }
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string img1, img2;
+
+            pictureCount = 0;
+            for (int i = 0; i < lvImages.Items.Count - 1; i++)
+            {
+                img1 = lvImages.Items[i].Text;
+                img2 = lvImages.Items[i + 1].Text;
+                lblProgress.Text = "Merging " + img1 + "...";
+                lblProgress.Refresh();
+                Debug.WriteLine($"Tweening {img1} and {img2}...");
+                //MergeImages(img1, img2);
+                TweenImages(img1, img2);
+                pic.Refresh();
+            }
+            lblProgress.Text = "Done!";
+        }
+
+        private void TweenImages(string image1, string image2)
+        {
+            FileStream fs1 = new System.IO.FileStream(image1, FileMode.Open, FileAccess.Read);
+            Image img1 = Image.FromStream(fs1);
+            Image img0 = (Image)img1.Clone();
+            pic.Image = img0;
+
+            FileStream fs2 = new System.IO.FileStream(image2, FileMode.Open, FileAccess.Read);
+            Image img2 = Image.FromStream(fs2);
+
+            int frames = (int)numTweenFrames.Value;
+            decimal multiplier = (decimal)(1.0 / frames);
+            int origWidth = img1.Width;
+            int origHeight = img1.Height;
+            int outerScaleX, outerScaleY, innerScaleX, innerScaleY;
+            int outerPosX, outerPosY, innerPosX, innerPosY;
+            double innerOpacity;
+            ImageAttributes opaque = new ImageAttributes();
+            ImageAttributes translucent = new ImageAttributes();
+            ColorMatrix opMatrix = new ColorMatrix();
+            ColorMatrix trMatrix = new ColorMatrix();
+
+            opMatrix.Matrix33 = 1; // transparency
+            opaque.SetColorMatrix(opMatrix);
+
+            for (int i = 0; i < frames; i++)
+            {
+                outerScaleX = (int)((origWidth * 2) - (origWidth * multiplier * i));
+                outerScaleY = (int)((origHeight * 2) - (origHeight * multiplier * i));
+                innerScaleX = (int)(origWidth - ((origWidth / 2) * multiplier * i));
+                innerScaleY = (int)(origHeight - ((origHeight / 2) * multiplier * i));
+                innerOpacity = (double)(1 - (multiplier * i));
+                outerPosX = (origWidth - outerScaleX) / 2;
+                outerPosY = (origHeight - outerScaleY) / 2; 
+                innerPosX = (origWidth - innerScaleX) / 2;
+                innerPosY = (origHeight - innerScaleY) / 2;
+
+                trMatrix.Matrix33 = (float)innerOpacity; // transparency
+                translucent.SetColorMatrix(trMatrix);
+
+                Debug.WriteLine($"Frame {pictureCount}: Outer image is {outerScaleX} x {outerScaleY} at ({outerPosX},{outerPosY})");
+                Debug.WriteLine($"...Inner image is {innerScaleX} x {innerScaleY} at ({innerPosX},{innerPosY})");
+
+                Graphics grMerge = Graphics.FromImage(pic.Image);
+                grMerge.DrawImage(img2, new Rectangle(outerPosX,outerPosY,outerScaleX,outerScaleY), 0, 0, origWidth, origHeight, GraphicsUnit.Pixel,opaque);
+                // actually, never mind blending the inner image. I think the outer works just fine.
+                //grMerge.DrawImage(img1, new Rectangle(innerPosX, innerPosY, innerScaleX, innerScaleY), 0, 0, origWidth, origHeight, GraphicsUnit.Pixel,translucent);
+
+                pic.Invalidate();
+                pic.Image.Save(destinationPath + "/" + txtPrefix.Text + "-" + frameNum(pictureCount) + ".jpg",ImageFormat.Jpeg);
+                pictureCount++;
+            }
+            fs1.Close();
+            fs2.Close();
+
+        }
+
+        private string frameNum(int frame)
+        {
+            string zeros = "000000";
+            return frame.ToString(zeros);
+        }
+
+        //private void MergeImages(string image1, string image2)
+        //{
+        //    FileStream fs = new System.IO.FileStream(image1, FileMode.Open, FileAccess.Read);
+        //    pic.Image = Image.FromStream(fs);
+        //    fs.Close();
+
+        //    fs = new System.IO.FileStream(image2, FileMode.Open, FileAccess.Read);
+        //    Image img2 = Image.FromStream(fs);
+        //    fs.Close();
+
+        //    ImageAttributes attributes = new ImageAttributes();
+        //    ColorMatrix matrix = new ColorMatrix();
+        //    matrix.Matrix33 = 0.5f; // transparency
+        //    attributes.SetColorMatrix(matrix);
+        //    pic.Invalidate();
+
+        //    Graphics grMerge = Graphics.FromImage(pic.Image);
+        //    grMerge.DrawImage(img2,
+        //        new Rectangle(0, 0, img2.Width, img2.Height),
+        //        0, 0, img2.Width, img2.Height,
+        //        GraphicsUnit.Pixel, attributes);
+
+        //    pic.Image.Save(image1.Replace(".jpg", "a.jpg"));
+        //}
+    }
+}
