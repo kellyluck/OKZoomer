94500673d9a5ed8b85abd49872fc5d2d45141b07
Kelly Luck <toymaker@usa.net>
2024-04-14 02:17:16 -0500

    Moved to background thread, changed form resize behavior, added resume-from-keyframe and completion info including ETA.

--- OKZoomer/Form1.Designer.cs
+++ OKZoomer/Form1.Designer.cs
@@ -39,15 +39,23 @@ namespace OKZoomer
             this.numTweenFrames = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtFrameStart = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Filename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numTweenFrames)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSelectFolder
             // 
-            this.btnSelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSelectFolder.Location = new System.Drawing.Point(3, 3);
             this.btnSelectFolder.Name = "btnSelectFolder";
-            this.btnSelectFolder.Size = new System.Drawing.Size(156, 23);
+            this.btnSelectFolder.Size = new System.Drawing.Size(297, 118);
             this.btnSelectFolder.TabIndex = 0;
             this.btnSelectFolder.Text = "Select Image Folder";
             this.btnSelectFolder.UseVisualStyleBackColor = true;
@@ -55,9 +63,10 @@ namespace OKZoomer
             // 
             // btnGo
             // 
-            this.btnGo.Location = new System.Drawing.Point(12, 415);
+            this.btnGo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnGo.Location = new System.Drawing.Point(3, 783);
             this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(156, 23);
+            this.btnGo.Size = new System.Drawing.Size(297, 27);
             this.btnGo.TabIndex = 1;
             this.btnGo.Text = "Go!";
             this.btnGo.UseVisualStyleBackColor = true;
@@ -65,37 +74,42 @@ namespace OKZoomer
             // 
             // lvImages
             // 
+            this.lvImages.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Filename});
+            this.lvImages.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lvImages.HideSelection = false;
-            this.lvImages.Location = new System.Drawing.Point(12, 41);
+            this.lvImages.Location = new System.Drawing.Point(3, 127);
             this.lvImages.Name = "lvImages";
-            this.lvImages.Size = new System.Drawing.Size(156, 368);
+            this.lvImages.Size = new System.Drawing.Size(297, 650);
             this.lvImages.Sorting = System.Windows.Forms.SortOrder.Ascending;
             this.lvImages.TabIndex = 2;
             this.lvImages.UseCompatibleStateImageBehavior = false;
-            this.lvImages.View = System.Windows.Forms.View.List;
+            this.lvImages.View = System.Windows.Forms.View.Details;
             // 
             // pic
             // 
-            this.pic.Location = new System.Drawing.Point(174, 87);
+            this.pic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic.Location = new System.Drawing.Point(306, 127);
             this.pic.Name = "pic";
-            this.pic.Size = new System.Drawing.Size(614, 322);
+            this.pic.Size = new System.Drawing.Size(891, 650);
             this.pic.TabIndex = 3;
             this.pic.TabStop = false;
             // 
             // lblProgress
             // 
             this.lblProgress.AutoSize = true;
-            this.lblProgress.Location = new System.Drawing.Point(515, 425);
+            this.lblProgress.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblProgress.Location = new System.Drawing.Point(306, 780);
             this.lblProgress.Name = "lblProgress";
-            this.lblProgress.Size = new System.Drawing.Size(273, 13);
+            this.lblProgress.Size = new System.Drawing.Size(891, 33);
             this.lblProgress.TabIndex = 4;
             this.lblProgress.Text = "Select the directory with the files and press \'Go\' to begin.";
-            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(209, 22);
+            this.label2.Location = new System.Drawing.Point(3, 8);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(125, 13);
             this.label2.TabIndex = 6;
@@ -104,7 +118,7 @@ namespace OKZoomer
             // 
             // numTweenFrames
             // 
-            this.numTweenFrames.Location = new System.Drawing.Point(340, 20);
+            this.numTweenFrames.Location = new System.Drawing.Point(134, 6);
             this.numTweenFrames.Name = "numTweenFrames";
             this.numTweenFrames.Size = new System.Drawing.Size(49, 20);
             this.numTweenFrames.TabIndex = 7;
@@ -117,7 +131,7 @@ namespace OKZoomer
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(251, 58);
+            this.label3.Location = new System.Drawing.Point(45, 44);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 8;
@@ -126,32 +140,85 @@ namespace OKZoomer
             // 
             // txtPrefix
             // 
-            this.txtPrefix.Location = new System.Drawing.Point(340, 55);
+            this.txtPrefix.Location = new System.Drawing.Point(134, 41);
             this.txtPrefix.Name = "txtPrefix";
             this.txtPrefix.Size = new System.Drawing.Size(100, 20);
             this.txtPrefix.TabIndex = 9;
             this.txtPrefix.Text = "test";
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.75F));
+            this.tableLayoutPanel1.Controls.Add(this.btnSelectFolder, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lvImages, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pic, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnGo, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblProgress, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.92357F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84.07643F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1200, 813);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtFrameStart);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtPrefix);
+            this.panel1.Controls.Add(this.numTweenFrames);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(306, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(891, 118);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtFrameStart
+            // 
+            this.txtFrameStart.Location = new System.Drawing.Point(362, 6);
+            this.txtFrameStart.Name = "txtFrameStart";
+            this.txtFrameStart.Size = new System.Drawing.Size(44, 20);
+            this.txtFrameStart.TabIndex = 11;
+            this.txtFrameStart.Text = "0";
+            this.txtFrameStart.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Resume at Frame #";
+            // 
+            // Filename
+            // 
+            this.Filename.Text = "File Name";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtPrefix);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.numTweenFrames);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblProgress);
-            this.Controls.Add(this.pic);
-            this.Controls.Add(this.lvImages);
-            this.Controls.Add(this.btnGo);
-            this.Controls.Add(this.btnSelectFolder);
+            this.ClientSize = new System.Drawing.Size(1200, 813);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "OK, Zoomer.";
             ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numTweenFrames)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -167,6 +234,11 @@ namespace OKZoomer
         private System.Windows.Forms.NumericUpDown numTweenFrames;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtFrameStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader Filename;
     }
 }
 
--- OKZoomer/Form1.cs
+++ OKZoomer/Form1.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OKZoomer
@@ -16,9 +12,14 @@ namespace OKZoomer
     public partial class Form1 : Form
     {
         string imagesSourcePath = @"F:\AIArt\output\img2img-images\2024-04-09\test";
-        string destinationPath = @"F:\AIArt\output\img2img-images\2024-04-09\test\out";
+        string destinationPath = @"F:\AIArt\output\img2img-images\2024-04-09\4x\out";
         List<String> sourceImages;
-        int pictureCount = 0;
+        int frameCount = 0;
+        private BackgroundWorker _worker;
+        int startFrame = 0;
+        DateTime startTime;
+        int totalFrames;
+        int tweenFrames;
 
         public Form1()
         {
@@ -38,6 +39,7 @@ namespace OKZoomer
 
         private void LoadImages(string imagePath)
         {
+            lblProgress.Text = "Loading images...";
             sourceImages = new List<string>();
             lvImages.Items.Clear();
             string[] fileEntries = Directory.GetFiles(imagePath);
@@ -46,30 +48,75 @@ namespace OKZoomer
                 sourceImages.Add(fileName);
                 lvImages.Items.Add(fileName);
             }
-
+            lblProgress.Text = $"{sourceImages.Count.ToString()} images loaded.";
+            lblProgress.Invalidate();
         }
 
         private void btnGo_Click(object sender, EventArgs e)
         {
+            int start = 0;
+            if (Int32.TryParse(txtFrameStart.Text, out start)) { startFrame = start; }
+            tweenFrames = (int)numTweenFrames.Value;
+            totalFrames = tweenFrames * (sourceImages.Count);
+
+            if (_worker != null)
+            {
+                _worker.Dispose();
+            }
+
+            _worker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+            _worker.DoWork += DoWork;
+            _worker.RunWorkerCompleted += RunWorkerCompleted;
+            _worker.ProgressChanged += ProgressChanged;
+            _worker.RunWorkerAsync();
+
+        }
+
+        private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pic.Refresh();
+            DateTime soFar = DateTime.Now;
+            double realStart = startFrame * tweenFrames;
+            double percent = 100 * ((double)((frameCount - realStart) / (totalFrames - realStart)));
+            double displayPercent = 100 * ((double)frameCount / totalFrames);
+            TimeSpan duration = soFar.Subtract(startTime);
+            TimeSpan toGo = new TimeSpan(0, 0, (int)((duration.TotalSeconds / percent) * (100 - percent)));
+            Debug.WriteLine($"real percent is {percent}, time passed is {duration.TotalSeconds}, sec per %: {duration.TotalSeconds / percent}, {100 - percent}% to go, so {toGo.TotalSeconds} seconds left.");
+            lblProgress.Text = $"Merging {e.UserState}, {frameCount} of {totalFrames}, {(int)displayPercent}%, {toGo.ToString("c")} to go.";
+        }
+
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblProgress.Text = "Done!";
+        }
+
+        private void DoWork(object sneder, DoWorkEventArgs e)
+        {
             string img1, img2;
 
-            pictureCount = 0;
-            for (int i = 0; i < lvImages.Items.Count - 1; i++)
+            frameCount = 0;
+            if (startFrame > 0) { frameCount = (int)numTweenFrames.Value * startFrame; }
+            startTime = DateTime.Now;
+
+            for (int i = startFrame; i < sourceImages.Count - 1; i++)
             {
-                img1 = lvImages.Items[i].Text;
-                img2 = lvImages.Items[i + 1].Text;
-                lblProgress.Text = "Merging " + img1 + "...";
-                lblProgress.Refresh();
+                img1 = sourceImages[i];
+                img2 = sourceImages[i + 1];
+
+                //lblProgress.Refresh();
                 Debug.WriteLine($"Tweening {img1} and {img2}...");
                 //MergeImages(img1, img2);
                 TweenImages(img1, img2);
-                pic.Refresh();
             }
-            lblProgress.Text = "Done!";
         }
 
         private void TweenImages(string image1, string image2)
         {
+            PictureBox pic = new PictureBox();
             FileStream fs1 = new System.IO.FileStream(image1, FileMode.Open, FileAccess.Read);
             Image img1 = Image.FromStream(fs1);
             Image img0 = (Image)img1.Clone();
@@ -95,30 +142,33 @@ namespace OKZoomer
 
             for (int i = 0; i < frames; i++)
             {
+                _worker.ReportProgress(i, image1);
+
                 outerScaleX = (int)((origWidth * 2) - (origWidth * multiplier * i));
                 outerScaleY = (int)((origHeight * 2) - (origHeight * multiplier * i));
                 innerScaleX = (int)(origWidth - ((origWidth / 2) * multiplier * i));
                 innerScaleY = (int)(origHeight - ((origHeight / 2) * multiplier * i));
                 innerOpacity = (double)(1 - (multiplier * i));
                 outerPosX = (origWidth - outerScaleX) / 2;
-                outerPosY = (origHeight - outerScaleY) / 2; 
+                outerPosY = (origHeight - outerScaleY) / 2;
                 innerPosX = (origWidth - innerScaleX) / 2;
                 innerPosY = (origHeight - innerScaleY) / 2;
 
                 trMatrix.Matrix33 = (float)innerOpacity; // transparency
                 translucent.SetColorMatrix(trMatrix);
 
-                Debug.WriteLine($"Frame {pictureCount}: Outer image is {outerScaleX} x {outerScaleY} at ({outerPosX},{outerPosY})");
-                Debug.WriteLine($"...Inner image is {innerScaleX} x {innerScaleY} at ({innerPosX},{innerPosY})");
+                //Debug.WriteLine($"Frame {pictureCount}: Outer image is {outerScaleX} x {outerScaleY} at ({outerPosX},{outerPosY})");
+                //Debug.WriteLine($"...Inner image is {innerScaleX} x {innerScaleY} at ({innerPosX},{innerPosY})");
 
                 Graphics grMerge = Graphics.FromImage(pic.Image);
-                grMerge.DrawImage(img2, new Rectangle(outerPosX,outerPosY,outerScaleX,outerScaleY), 0, 0, origWidth, origHeight, GraphicsUnit.Pixel,opaque);
+                grMerge.DrawImage(img2, new Rectangle(outerPosX, outerPosY, outerScaleX, outerScaleY), 0, 0, origWidth, origHeight, GraphicsUnit.Pixel, opaque);
                 // actually, never mind blending the inner image. I think the outer works just fine.
                 //grMerge.DrawImage(img1, new Rectangle(innerPosX, innerPosY, innerScaleX, innerScaleY), 0, 0, origWidth, origHeight, GraphicsUnit.Pixel,translucent);
 
                 pic.Invalidate();
-                pic.Image.Save(destinationPath + "/" + txtPrefix.Text + "-" + frameNum(pictureCount) + ".jpg",ImageFormat.Jpeg);
-                pictureCount++;
+                pic.Image.Save(destinationPath + "/" + txtPrefix.Text + "-" + frameNum(frameCount) + ".jpg", ImageFormat.Jpeg);
+                frameCount++;
+                GC.Collect();
             }
             fs1.Close();
             fs2.Close();
@@ -129,6 +179,11 @@ namespace OKZoomer
         {
             string zeros = "000000";
             return frame.ToString(zeros);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
 
         //private void MergeImages(string image1, string image2)
